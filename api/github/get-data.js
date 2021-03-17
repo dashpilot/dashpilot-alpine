73dd9a96d7d6ebbb71f07d5bd696a20106627619
Dashpilot <39771945+dashpilot@users.noreply.github.com>
2021-03-17 04:57:26 +0100

    Update get-data.js

--- api/github/get-data.js
+++ api/github/get-data.js
@@ -1,5 +1,5 @@
 import Status from 'http-status-codes';
-import verifyToken from "../auth/firebase.js";
+import verifyToken from "../auth/userbase.js";
 import {
   getData
 } from "./lib/github.js";
